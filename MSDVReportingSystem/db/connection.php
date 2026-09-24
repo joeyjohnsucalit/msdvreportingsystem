4da7d7dd46908f60ace5883245e20c0d10d2d85d
joeyjohnsucalit <joeyjohnsucalit23@gmail.com>
2026-09-24 07:06:17 -0700

    Update connection.php

--- MSDVReportingSystem/db/connection.php
+++ MSDVReportingSystem/db/connection.php
@@ -11,6 +11,7 @@ $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::MYSQL_ATTR_INIT_COMMAND => "SET SESSION sql_mode = REPLACE(@@sql_mode, 'ONLY_FULL_GROUP_BY', '')",
+     PDO::ATTR_EMULATE_PREPARES   => true,  
 ];
 
 // TiDB Cloud requires a secure (TLS) connection
